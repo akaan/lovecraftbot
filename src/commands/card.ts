ad6b94e5ca9140ac515d60c59369648ca828c75b
Akaan Qualrus <akaan.qualrus@gmail.com>
2021-03-05 09:16:32 +0100

    Recherche de carte par titre et niveau

--- src/commands/card.ts
+++ src/commands/card.ts
@@ -1,3 +1,4 @@
+import * as Discord from "discord.js";
 import { Inject } from "typescript-ioc";
 
 import { ICommand, ICommandArgs, ICommandResult } from "../interfaces";
@@ -8,27 +9,99 @@ export class CardCommand implements ICommand {
   aliases = ["!", "c", "card", "carte"];
 
   @Inject private cardService: CardService;
-  private CARD_ID_REGEX = /\d{5}/;
+  private CARD_ID_REGEX = /\d{5}$/;
+  private CARD_AND_XP_REGEX = /(\D*)(?:\s(\d))?$/;
+
+  private async sendSingleCardImageLink(
+    message: Discord.Message,
+    cardId: string
+  ): Promise<ICommandResult> {
+    const imageUrl = await this.cardService.getCardImageLink(cardId);
+    if (imageUrl) {
+      await message.reply(imageUrl);
+      return {
+        resultString: `CardCommand: image envoyée.`,
+      };
+    } else {
+      await message.reply(`Je n'ai pas trouvé d'image pour la carte ${cardId}`);
+      return {
+        resultString: `CardCommand: image non trouvée pour l'ID ${cardId}`,
+      };
+    }
+  }
+
+  private async sendCardImageLinks(
+    message: Discord.Message,
+    cardIds: string[]
+  ): Promise<ICommandResult> {
+    const maybeLinks = await Promise.all(
+      cardIds.map((id) => this.cardService.getCardImageLink(id))
+    );
+    const links = maybeLinks.filter((l) => l !== undefined);
+    if (links.length > 0) {
+      await Promise.all(links.map((l) => message.reply(l)));
+      return { resultString: `CardCommand: images envoyées` };
+    } else {
+      await message.reply(`je n'ai trouvé aucune image pour ces cartes.`);
+      return {
+        resultString: `CardCommand: aucune image trouvée pour les ID ${cardIds.join(
+          ", "
+        )}`,
+      };
+    }
+  }
 
   async execute(cmdArgs: ICommandArgs): Promise<ICommandResult> {
     const { message, args } = cmdArgs;
 
     const maybeCardId = this.CARD_ID_REGEX.exec(args);
     if (maybeCardId) {
-      const cardId = maybeCardId[0];
-      const imageUrl = await this.cardService.getCardImageLink(cardId);
-      if (imageUrl) {
-        await message.reply(imageUrl);
-        return { resultString: "card: image envoyée" };
+      // Recherche par ID de carte
+      return this.sendSingleCardImageLink(message, maybeCardId[0]);
+    }
+
+    // Recherche par titre de carte
+    const [, searchString, maybeXpAsString] = this.CARD_AND_XP_REGEX.exec(args);
+    const cardsForTitle = await this.cardService.getCardsForTitle(
+      searchString.trim()
+    );
+
+    if (cardsForTitle.length === 0) {
+      await message.reply("désolé, le mystère de cette carte reste entier.");
+      return {
+        resultString: `CardCommand: Aucune carte correspondant à "${searchString}"`,
+      };
+    }
+
+    if (maybeXpAsString && maybeXpAsString !== "0") {
+      // La première carte avec le niveau d'XP précisé
+      const maybeCardWithGivenXp = cardsForTitle.find(
+        (c) => c.xp === parseInt(maybeXpAsString, 10)
+      );
+      if (maybeCardWithGivenXp) {
+        return this.sendSingleCardImageLink(message, maybeCardWithGivenXp.id);
       } else {
         await message.reply(
-          `Je n'ai pas trouvé d'image pour la carte ${cardId}`
+          `je n'ai pas trouvé de carte de niveau ${maybeXpAsString} correspondant.`
         );
-        return { resultString: "card: image non trouvée" };
+        return {
+          resultString: `CardCommand: Aucune carte de niveau ${maybeXpAsString} correspondant à "${searchString}"`,
+        };
       }
-    } else {
-      await message.reply("Je n'ai pas compris");
-      return { resultString: "card: pas d'ID de carte précisé" };
     }
+
+    if (maybeXpAsString && maybeXpAsString === "0") {
+      // Tous les niveaux de la première carte trouvée
+      const allCards = cardsForTitle.filter(
+        (c) => c.title === cardsForTitle[0].title
+      );
+      return this.sendCardImageLinks(
+        message,
+        allCards.map((c) => c.id)
+      );
+    }
+
+    // La première des cartes trouvées pour ce titre
+    return this.sendSingleCardImageLink(message, cardsForTitle[0].id);
   }
 }
--- src/services/card.ts
+++ src/services/card.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
+import * as cheerio from "cheerio";
 import { OnlyInstantiableByContainer, Singleton } from "typescript-ioc";
 import { BaseService } from "../base/BaseService";
+
+interface CardData {
+  title: string;
+  xp?: number;
+  id: string;
+}
 
 @Singleton
 @OnlyInstantiableByContainer
@@ -12,6 +19,34 @@ export class CardService extends BaseService {
       `http://arkhamdb.com/bundles/cards/${cardId}.png`,
     ];
     return this.getFirstWorkingUrl(possibleUrls);
+  }
+
+  public async getCardsForTitle(searchString: string): Promise<CardData[]> {
+    const escaped = encodeURIComponent(searchString);
+    const url = `http://arkhamdb.fr.cr/recherche/${escaped}`;
+    const searchResultPage = await axios.get<string>(url);
+    const $ = cheerio.load(searchResultPage.data);
+
+    const REGEX = /^([^(]+)(?: \((\d)\))?$/;
+
+    const cards = $("table.tableau")
+      // C'est toujours le dernier tableau qui contient la recherche par titre
+      .last()
+      .find("tbody tr")
+      .map((_i, row) => {
+        const cardTitleAndLink = $(row).find("td").first();
+        const cardTitleAndXp = cardTitleAndLink.text().trim();
+        const cardLink = cardTitleAndLink.find("a").attr("href");
+        const [, cardTitle, maybeCardXp] = REGEX.exec(cardTitleAndXp);
+        const parsedXp = parseInt(maybeCardXp, 10);
+        return {
+          title: cardTitle,
+          xp: isNaN(parsedXp) ? undefined : parsedXp,
+          id: cardLink.split("/")[4],
+        } as CardData;
+      })
+      .get();
+    return cards as CardData[];
   }
 
   private getFirstWorkingUrl(urls: string[]): Promise<string | undefined> {
