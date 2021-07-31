07525fae4565048bbf40cdcbdfcfb72f51c5a47c
Akaan Qualrus <akaan.qualrus@gmail.com>
2021-07-31 20:55:33 +0200

    Ajout ressources de guilde

--- src/services/ResourcesService.ts
+++ src/services/ResourcesService.ts
@@ -3,10 +3,12 @@ import * as util from "util";
 import { OnlyInstantiableByContainer, Singleton, Inject } from "typescript-ioc";
 import { BaseService } from "../base/BaseService";
 import { LoggerService } from "./LoggerService";
+import { Guild } from "discord.js";
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const exists = util.promisify(fs.exists);
+const mkdir = util.promisify(fs.mkdir);
 
 @Singleton
 @OnlyInstantiableByContainer
@@ -28,5 +30,51 @@ export class ResourcesService extends BaseService {
     return writeFile(`./data/${filename}`, content).catch((err) => {
       this.logger.error(err);
     });
+  }
+
+  public guildResourceExists(guild: Guild, filename: string): Promise<boolean> {
+    return ResourcesService.createGuildFolder(guild).then(() => {
+      return exists(`./data/${filename}`);
+    });
+  }
+
+  public readGuildResource(
+    guild: Guild,
+    filename: string
+  ): Promise<string | undefined> {
+    return ResourcesService.createGuildFolder(guild).then(() => {
+      return readFile(`./data/guild-${guild.id}/${filename}`, "utf-8").catch(
+        (err) => {
+          this.logger.error(err);
+          return undefined as string | undefined;
+        }
+      );
+    });
+  }
+
+  public saveGuildResource(
+    guild: Guild,
+    filename: string,
+    content: string
+  ): Promise<void> {
+    return ResourcesService.createGuildFolder(guild).then(() => {
+      return writeFile(`./data/guild-${guild.id}/${filename}`, content).catch(
+        (err) => {
+          this.logger.error(err);
+        }
+      );
+    });
+  }
+
+  private static async createGuildFolder(guild: Guild): Promise<void> {
+    try {
+      return await mkdir(`./data/guild-${guild.id}`);
+    } catch (err) {
+      if ((err as NodeJS.ErrnoException).code === "EEXIST") {
+        return Promise.resolve();
+      } else {
+        return Promise.reject();
+      }
+    }
   }
 }
