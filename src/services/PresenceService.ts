a5c61304677cd42e557280abb93fa6e81c11b5b3
Akaan Qualrus <akaan.qualrus@gmail.com>
2021-12-12 19:06:17 +0100

    Message de présence au hasard

--- src/services/PresenceService.ts
+++ src/services/PresenceService.ts
@@ -3,11 +3,23 @@ import * as Discord from "discord.js";
 
 import { BaseService } from "../base/BaseService";
 import { EnvService } from "./EnvService";
+import { RandomService } from "./RandomService";
+
+const GAMES = [
+  "Horreur à Arkham JCE",
+  "Les Demeures de l'Épouvante",
+  "L'Insondable",
+  "Horreur à Arkham",
+  "Les Contrées de l'Horreur",
+  "Dernière Heure",
+  "Le Signe des Anciens",
+];
 
 @Singleton
 @OnlyInstantiableByContainer
 export class PresenceService extends BaseService {
   @Inject private envService!: EnvService;
+  @Inject private randomService!: RandomService;
 
   public async init(client: Discord.Client): Promise<void> {
     await super.init(client);
@@ -15,7 +27,16 @@ export class PresenceService extends BaseService {
     if (this.envService.ignorePresence) {
       return;
     }
-    this.setPresence("avec les différents jeux de la gamme");
+    this.setRandomPresence();
+    setInterval(() => {
+      this.setRandomPresence();
+    }, 1000 * 60);
+  }
+
+  public setRandomPresence(): void {
+    const activity =
+      GAMES[this.randomService.getRandomInt(0, GAMES.length - 1)];
+    this.setPresence(activity);
   }
 
   public setPresence(presence: string): void {
