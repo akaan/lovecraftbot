0e8681e2019664240e9f493efcd48760b93d3dd8
Akaan Qualrus <akaan.qualrus@gmail.com>
2021-12-12 10:22:35 +0100

    Regroupement des commandes de timing

---
+++ src/commands/TimingCommand.ts
@@ -0,0 +1,45 @@
+import { CommandInteraction } from "discord.js";
+// eslint-disable-next-line import/no-unresolved
+import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
+import { ISlashCommand, ISlashCommandResult } from "../interfaces";
+
+export class TimingCommand implements ISlashCommand {
+  isAdmin = false;
+  name = "t";
+  description = "Affiche un timing";
+  options = [
+    {
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      name: "phases",
+      description: "Affiche le timing des phases du jeu",
+    },
+    {
+      type: ApplicationCommandOptionTypes.SUB_COMMAND,
+      name: "test",
+      description: "Affiche le timing d'un test",
+    },
+  ];
+
+  async execute(
+    commandInteraction: CommandInteraction
+  ): Promise<ISlashCommandResult> {
+    if (commandInteraction.options.getSubcommand() === "phases") {
+      await commandInteraction.reply({ files: ["assets/phase.jpg"] });
+      return {
+        message: "[TimingCommand] Timing des phases envoyé",
+      };
+    }
+
+    if (commandInteraction.options.getSubcommand() === "test") {
+      await commandInteraction.reply({ files: ["assets/timing.jpg"] });
+      return {
+        message: "[TimingCommand] Timing d'un test envoyé",
+      };
+    }
+
+    await commandInteraction.reply(`Oops, il y a eu un problème`);
+    return {
+      message: `[TimingCommand] Sous-commandes ${commandInteraction.options.getSubcommand()} inconnue`,
+    };
+  }
+}
--- src/commands/slashCommands.ts
+++ src/commands/slashCommands.ts
@@ -5,7 +5,6 @@ export * from "./DeckCommand";
 export * from "./EchoCommand";
 export * from "./EnglishCommand";
 export * from "./FrenchCommand";
-export * from "./PhaseTimingCommand";
 export * from "./RefreshCommand";
 export * from "./RuleCommand";
-export * from "./SkillTestTimingCommand";
+export * from "./TimingCommand";
