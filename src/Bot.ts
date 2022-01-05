f525d542c2b07dfd04c7c9a7849f8370062e3e3f
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-05 09:12:38 +0100

    refactor(Bot): déplacement écouteurs

--- src/Bot.ts
+++ src/Bot.ts
@@ -18,7 +18,8 @@ import { RulesService } from "./services/RulesService";
 export class Bot {
   private static LOG_LABEL = "Bot";
 
-  private client?: Discord.Client;
+  private client!: Discord.Client;
+  private commandPrefix!: string;
 
   @Inject private blobGameService!: BlobGameService;
   @Inject private cardOfTheDayService!: CardOfTheDayService;
@@ -41,7 +42,7 @@ export class Bot {
       throw new Error("No Discord token specified!");
     }
 
-    const COMMAND_PREFIX = this.envService.commandPrefix;
+    this.commandPrefix = this.envService.commandPrefix;
 
     this.client = new Discord.Client({
       intents: [
@@ -50,131 +51,145 @@ export class Bot {
         Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
       ],
     });
-    const client = this.client;
+    this.client.on("ready", () => this.handleReady());
+    this.client.on("interactionCreate", (interaction) =>
+      this.handleInteraction(interaction)
+    );
+    this.client.on("messageCreate", (msg) => this.handleMessage(msg));
+    this.client.on("messageReactionAdd", (reaction, user) =>
+      this.handleAddReaction(reaction, user)
+    );
+    this.client.on("messageReactionRemove", (reaction, user) =>
+      this.handleRemoveReaction(reaction, user)
+    );
+
     this.logger.info(Bot.LOG_LABEL, "Connexion à Discord ...");
-
-    this.client.on("ready", () => {
-      this.logger.info(Bot.LOG_LABEL, "Connecté.");
-
-      [
-        this.logger,
-        this.presenceService,
-        this.emojiService,
-        this.cardService,
-        this.cardOfTheDayService,
-        this.newsService,
-        this.rulesService,
-        this.blobGameService,
-        this.massMultiplayerEventService,
-        this.commandParser,
-        this.applicationCommandManager,
-      ].map((service) => {
-        service
-          .init(client)
-          .then(() => {
-            this.logger.info(
-              Bot.LOG_LABEL,
-              `Service ${nameOfConstructor(service)} initialisé`
-            );
-          })
-          .catch((err) => {
-            this.logger.error(
-              Bot.LOG_LABEL,
-              `Problème à l'initialisation du service
-              ${nameOfConstructor(service)}`,
-              { error: err }
-            );
-          });
-      });
-    });
-
-    this.client.on("interactionCreate", (interaction) => {
-      if (interaction.isCommand()) {
-        this.applicationCommandManager
-          .handleCommandInteraction(interaction)
-          .then((applicationCommandResult) => {
-            this.logger.info(
-              Bot.LOG_LABEL,
-              `Commande d'application traitée`,
-              applicationCommandResult
-            );
-          })
-          .catch((err) =>
-            this.logger.error(
-              Bot.LOG_LABEL,
-              `Erreur au traitement d'une commande d'application`,
-              { error: err }
-            )
-          );
-      }
-    });
-
-    this.client.on("messageCreate", (msg) => {
-      if (
-        msg.author.bot ||
-        (this.client &&
-          this.client.user &&
-          msg.author.id === this.client.user.id)
-      ) {
-        return;
-      }
-
-      if (
-        this.envService.testServerId &&
-        msg.guild &&
-        this.envService.testServerId !== msg.guild.id
-      ) {
-        return;
-      }
-
-      const content = msg.content;
-
-      if (content.startsWith(COMMAND_PREFIX)) {
-        this.commandParser
-          .handleCommand(msg)
-          .then((result) =>
-            this.logger.info(Bot.LOG_LABEL, "Commande classique traitée", {
-              result,
-            })
-          )
-          .catch((err) =>
-            this.logger.error(
-              Bot.LOG_LABEL,
-              "Erreur au traitement d'une commande classique",
-              {
-                error: err,
-              }
-            )
-          );
-      } else {
-        this.commandParser.handleMessage(msg);
-      }
-    });
-
-    this.client.on("messageReactionAdd", (reaction, user) => {
-      if (user.bot) {
-        return;
-      }
-
-      this.commandParser.handleEmojiAdd(
-        reaction as Discord.MessageReaction,
-        user
-      );
-    });
-
-    this.client.on("messageReactionRemove", (reaction, user) => {
-      if (user.bot) {
-        return;
-      }
-
-      this.commandParser.handleEmojiRemove(
-        reaction as Discord.MessageReaction,
-        user
-      );
-    });
-
     await this.client.login(DISCORD_TOKEN);
     return;
+  }
+
+  private handleReady(): void {
+    this.logger.info(Bot.LOG_LABEL, "Connecté.");
+
+    [
+      this.logger,
+      this.presenceService,
+      this.emojiService,
+      this.cardService,
+      this.cardOfTheDayService,
+      this.newsService,
+      this.rulesService,
+      this.blobGameService,
+      this.massMultiplayerEventService,
+      this.commandParser,
+      this.applicationCommandManager,
+    ].map((service) => {
+      service
+        .init(this.client)
+        .then(() => {
+          this.logger.info(
+            Bot.LOG_LABEL,
+            `Service ${nameOfConstructor(service)} initialisé`
+          );
+        })
+        .catch((err) => {
+          this.logger.error(
+            Bot.LOG_LABEL,
+            `Problème à l'initialisation du service
+            ${nameOfConstructor(service)}`,
+            { error: err }
+          );
+        });
+    });
+  }
+
+  private handleInteraction(interaction: Discord.Interaction): void {
+    if (interaction.isCommand()) {
+      this.applicationCommandManager
+        .handleCommandInteraction(interaction)
+        .then((applicationCommandResult) => {
+          this.logger.info(
+            Bot.LOG_LABEL,
+            `Commande d'application traitée`,
+            applicationCommandResult
+          );
+        })
+        .catch((err) =>
+          this.logger.error(
+            Bot.LOG_LABEL,
+            `Erreur au traitement d'une commande d'application`,
+            { error: err }
+          )
+        );
+    }
+  }
+
+  private handleMessage(msg: Discord.Message): void {
+    if (
+      msg.author.bot ||
+      (this.client && this.client.user && msg.author.id === this.client.user.id)
+    ) {
+      return;
+    }
+
+    if (
+      this.envService.testServerId &&
+      msg.guild &&
+      this.envService.testServerId !== msg.guild.id
+    ) {
+      return;
+    }
+
+    const content = msg.content;
+
+    if (content.startsWith(this.commandPrefix)) {
+      this.commandParser
+        .handleCommand(msg)
+        .then((result) =>
+          this.logger.info(Bot.LOG_LABEL, "Commande classique traitée", {
+            result,
+          })
+        )
+        .catch((err) =>
+          this.logger.error(
+            Bot.LOG_LABEL,
+            "Erreur au traitement d'une commande classique",
+            {
+              error: err,
+            }
+          )
+        );
+    } else {
+      this.commandParser.handleMessage(msg);
+    }
+  }
+
+  private handleAddReaction(
+    reaction: Discord.MessageReaction | Discord.PartialMessageReaction,
+    user: Discord.User | Discord.PartialUser
+  ): void {
+    if (user.bot) {
+      return;
+    }
+
+    this.commandParser.handleEmojiAdd(
+      reaction as Discord.MessageReaction,
+      user
+    );
+  }
+
+  private handleRemoveReaction(
+    reaction: Discord.MessageReaction | Discord.PartialMessageReaction,
+    user: Discord.User | Discord.PartialUser
+  ): void {
+    if (user.bot) {
+      return;
+    }
+
+    this.commandParser.handleEmojiRemove(
+      reaction as Discord.MessageReaction,
+      user
+    );
   }
 
   public shutdown(): Promise<void> {
