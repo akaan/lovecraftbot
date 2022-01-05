8014b4df36d7d32fa7b5e07a723984ae2f9aa4c5
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-05 18:05:05 +0100

    doc(EnvService): ajout doc

--- src/services/EnvService.ts
+++ src/services/EnvService.ts
@@ -4,27 +4,50 @@ import { BaseService } from "../base/BaseService";
 
 @Singleton
 @OnlyInstantiableByContainer
+/**
+ * Service permettant au bot d'accéder à des variables d'environnement.
+ */
 export class EnvService extends BaseService {
+  /**
+   * Le jeton de connexion à Discord.
+   */
   public get discordToken(): string | undefined {
     return process.env.DISCORD_TOKEN;
   }
 
+  /**
+   * Le préfixe pour le lancement des commandes classiques.
+   */
   public get commandPrefix(): string {
     return process.env.COMMAND_PREFIX || "!";
   }
 
+  /**
+   * Indique s'il faut ignorer le positionnement d'un message de présence
+   * du bot.
+   */
   public get ignorePresence(): boolean {
     return !!process.env.IGNORE_PRESENCE;
   }
 
+  /**
+   * Un éventuel identifiant de serveur de test pour limiter l'activité du
+   * bot à ce serveur en particulier.
+   */
   public get testServerId(): string | undefined {
     return process.env.TEST_SERVER;
   }
 
+  /**
+   * Identifiant du canal sur lequel envoyer la carte du jour.
+   */
   public get cardOfTheDayChannelId(): string | undefined {
     return process.env.CARD_OF_THE_DAY_CHANNEL;
   }
 
+  /**
+   * Heure du jour à laquelle envoyer la carte du jour.
+   */
   public get cardOfTheDayHour(): number {
     const parsed = parseInt(process.env.CARD_OF_THE_DAY_HOUR || "8", 10);
     if (isNaN(parsed)) {
@@ -34,14 +57,19 @@ export class EnvService extends BaseService {
     }
   }
 
+  /**
+   * Nom de la catégorie de canaux où sont gérés les événements multijoueurs
+   */
   public get massMultiplayerEventCategoryName(): string | undefined {
     return process.env.MASS_MULTIPLAYER_EVENT_CATEGORY;
   }
 
+  /** Nom du canal réservé aux organisateurs d'un événement multijoueurs */
   public get massMultiplayerEventAdminChannelName(): string | undefined {
     return process.env.MASS_MULTIPLAYER_EVENT_ADMIN_CHANNEL;
   }
 
+  /** Nom du rôle ayant accès aux commandes d'administration du bot */
   public get botAdminRoleName(): string | undefined {
     return process.env.BOT_ADMIN_ROLE;
   }
