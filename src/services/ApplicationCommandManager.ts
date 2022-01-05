f1f51a18b5ca0467d48df9a0ca8c7c528e66dc95
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-05 15:37:18 +0100

    doc(ApplicationCommandManager): ajout doc

--- src/services/ApplicationCommandManager.ts
+++ src/services/ApplicationCommandManager.ts
@@ -20,20 +20,30 @@ import {
 import { EnvService } from "./EnvService";
 import { LoggerService } from "./LoggerService";
 
+/**
+ * Type représentant un dictionnaire de définitions de commandes d'application
+ */
 type ApplicationCommandsDictionary = {
   [key: string]: ApplicationCommandConstructor;
 };
+
+/** Ensembles des définitions  de commandes d'application importées */
 const AvailableApplicationCommands =
   ApplicationCommands as unknown as ApplicationCommandsDictionary;
 
 @Singleton
 @OnlyInstantiableByContainer
+/**
+ * Service gérant et exécutant les commandes d'application.
+ */
 export class ApplicationCommandManager extends BaseService {
+  /** Etiquette utilisée pour les logs de ce service */
   private static LOG_LABEL = "ApplicationCommandManager";
 
   @Inject private envService!: EnvService;
   @Inject logger!: LoggerService;
 
+  /** Liste des commandes d'application exécutables */
   private applicationCommands: IApplicationCommand[] = [];
 
   public async init(client: Client): Promise<void> {
@@ -41,6 +51,13 @@ export class ApplicationCommandManager extends BaseService {
     this.loadApplicationCommands(AvailableApplicationCommands);
   }
 
+  /**
+   * Gère une interaction en cherchant s'il existe une commande d'application
+   * associée et en l'exécutant si c'est le cas.
+   *
+   * @param commandInteraction Interation déclenchée par l'utilisateur
+   * @returns Promesse d'un résultat de commande d'application
+   */
   public handleCommandInteraction(
     commandInteraction: CommandInteraction
   ): Promise<IApplicationCommandResult> {
@@ -57,6 +74,13 @@ export class ApplicationCommandManager extends BaseService {
     }
   }
 
+  /**
+   * Instancie et référence les commandes d'application depuis leurs
+   * définitions.
+   *
+   * @param applicationCommands Dictionnaire des définitions de
+   *                            commandes d'application
+   */
   private loadApplicationCommands(
     applicationCommands: ApplicationCommandsDictionary
   ): void {
@@ -71,19 +95,39 @@ export class ApplicationCommandManager extends BaseService {
     );
   }
 
+  /**
+   * Renvoie les commandes d'application référencées au niveau serveur
+   *
+   * @returns Les commandes d'application de niveau serveur
+   */
   private getGuildApplicationCommands(): IApplicationCommand[] {
     return this.applicationCommands.filter((sc) => sc.isGuildCommand);
   }
 
+  /**
+   * Renvoie les commandes d'applications référencées au niveau global
+   *
+   * @returns Les commandes d'application globales
+   */
   public getGlobalApplicationCommands(): IApplicationCommand[] {
     return this.applicationCommands.filter((sc) => !sc.isGuildCommand);
   }
 
+  /**
+   * Référence auprès de Discord l'ensemble des commandes d'application
+   *
+   * @returns Une promesse résolue une fois les commandes référencées
+   */
   public async registerApplicationCommands(): Promise<void> {
     await this.registerGlobalApplicationCommands();
     await this.registerGuildApplicationCommands();
   }
 
+  /**
+   * Référence auprès de Discord les commandes d'application globales
+   *
+   * @returns Une promesse résolue une fois les commandes référencées
+   */
   private async registerGlobalApplicationCommands(): Promise<void> {
     if (this.client && this.client.application) {
       try {
@@ -108,6 +152,11 @@ export class ApplicationCommandManager extends BaseService {
     }
   }
 
+  /**
+   * Référence auprès de Discord les commandes d'application de niveau serveur
+   *
+   * @returns Une promesse résolue une fois les commandes référencées
+   */
   private async registerGuildApplicationCommands(): Promise<void> {
     if (this.client) {
       const guilds = this.client.guilds.cache.filter(
@@ -128,6 +177,12 @@ export class ApplicationCommandManager extends BaseService {
     }
   }
 
+  /**
+   * Référence auprès de Discord les permissions associées aux commandes
+   * d'application de niveau serveur
+   *
+   * @returns Une promesse résolue une fois les commandes référencées
+   */
   public async setGuildApplicationCommandsPermissions(): Promise<void> {
     if (!this.client) {
       return;
@@ -168,6 +223,11 @@ export class ApplicationCommandManager extends BaseService {
     }
   }
 
+  /**
+   * Déréférence les commandes d'application auprès de Discord.
+   *
+   * @returns Une promesse résolue une fois le déférencement effectué
+   */
   public async unregisterApplicationCommands(): Promise<void> {
     if (!this.client) return;
 
@@ -190,6 +250,15 @@ export class ApplicationCommandManager extends BaseService {
   }
 }
 
+/**
+ * Construit un filtre qui sera soit un passe-plat soit un filtre qui ne laisse
+ * passer que le serveur dont l'identifiant est celui fournit.
+ * Utile pour filtrer sur un serveur de test si un identifiant de serveur de
+ * test est présent.
+ *
+ * @param maybeGuildId Un identifiant de serveur ou rien s'il ne faut pas filtrer
+ * @returns Un filtre à appliquer sur une collection de serveurs
+ */
 function filterGuilds(
   maybeGuildId: string | undefined
 ): (guild: Guild) => boolean {
@@ -199,6 +268,12 @@ function filterGuilds(
   return (_guild: Guild) => true;
 }
 
+/**
+ * Construit une permission de type rôle.
+ *
+ * @param role Le rôle concerné par la permission
+ * @returns Le descriptif d'une permission basée sur le rôle
+ */
 function createRolePermission(role: Role): ApplicationCommandPermissionData {
   return {
     id: role.id,
@@ -207,6 +282,13 @@ function createRolePermission(role: Role): ApplicationCommandPermissionData {
   };
 }
 
+/**
+ * Construit une permission pour une commande d'application.
+ *
+ * @param command La commande concernée par la permission
+ * @param permissions Les permissions à associer à la commande
+ * @returns Une descriptif de permission pour une commande
+ */
 function createCommandPermission(
   command: ApplicationCommand,
   permissions: ApplicationCommandPermissionData[]
@@ -217,6 +299,15 @@ function createCommandPermission(
   };
 }
 
+/**
+ * Référence auprès de Discord (en annule et remplace), pour un serveur donné,
+ * des permissions pour une liste de commandes donnée et pour le rôlé donné.
+ *
+ * @param guild Le serveur sur lequel positionner la permission
+ * @param commandNames Les noms des commandes sur lesquelles appliquer la permission
+ * @param roleName Le nom du rôle auquel restreindre les commandes
+ * @returns Une promesse résolue une fois les permissions référencées
+ */
 async function allowCommandsForRoleName(
   guild: Guild,
   commandNames: string[],
@@ -224,7 +315,8 @@ async function allowCommandsForRoleName(
 ): Promise<void> {
   const role = guild.roles.cache.find((r) => r.name === roleName);
   if (role) {
-    // We need fetch because we just created them
+    // Il faut utiliser `fetch` car potentiellement les commandes viennent
+    // tout juste d'être créées et ne sont pas dans le cache.
     const commands = (await guild.commands.fetch())
       .filter((c) => commandNames.includes(c.name))
       .map((c) => c);
