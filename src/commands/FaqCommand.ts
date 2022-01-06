f930e6b2608f3672b734cc9cde215189ba49904c
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-06 21:31:00 +0100

    refactor(FaqCommand): extraction fonctions

--- src/commands/FaqCommand.ts
+++ src/commands/FaqCommand.ts
@@ -4,7 +4,7 @@ import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
 import { Inject } from "typescript-ioc";
 
 import { IApplicationCommand, IApplicationCommandResult } from "../interfaces";
-import { CardService, SearchType } from "../services/CardService";
+import { ArkhamDBCard, CardService, SearchType } from "../services/CardService";
 import { caseOfLength } from "../utils";
 
 /**
@@ -36,8 +36,6 @@ export class FaqCommand implements IApplicationCommand {
     commandInteraction: CommandInteraction
   ): Promise<IApplicationCommandResult> {
     const search = commandInteraction.options.getString("recherche");
-    const ephemeral =
-      commandInteraction.options.getBoolean("ephemere") || false;
 
     if (search) {
       const searchType = CardService.CARD_CODE_REGEX.test(search)
@@ -50,38 +48,10 @@ export class FaqCommand implements IApplicationCommand {
       });
 
       return caseOfLength(foundCards, {
-        ifOne: async (theCard) => {
-          const faqEntries = await this.cardService.getCardFAQ(theCard);
-          if (faqEntries.length > 0) {
-            await commandInteraction.reply({
-              content: faqEntries[0].text,
-              ephemeral,
-            });
-            return this.commandResult("FAQ envoyée");
-          } else {
-            await commandInteraction.reply({
-              content: `Aucune entrée de FAQ pour la carte ${theCard.name}`,
-              ephemeral: true,
-            });
-            return this.commandResult("Aucune FAQ pour cette carte");
-          }
-        },
-        ifMany: async (_allCards) => {
-          await commandInteraction.reply({
-            content: "Je ne sais pas encore faire cela",
-            ephemeral: true,
-          });
-          return this.commandResult("Non supporté");
-        },
-        ifEmpty: async () => {
-          await commandInteraction.reply({
-            content: "Aucune carte ne correspond à cette recherche",
-            ephemeral: true,
-          });
-          return this.commandResult(
-            `Aucune carte ne correspond à la recherche ${search}`
-          );
-        },
+        ifOne: (theCard) => this.sendFaqEntries(commandInteraction, theCard),
+        ifMany: (allCards) =>
+          this.sendCardChoicesThenFaqEntries(commandInteraction, allCards),
+        ifEmpty: () => this.noCardMatched(commandInteraction),
       });
     } else {
       await commandInteraction.reply({
@@ -90,6 +60,72 @@ export class FaqCommand implements IApplicationCommand {
       });
       return this.commandResult("Texte recherché non fourni");
     }
+  }
+
+  /**
+   * Pour envoyer les entrées de FAQ concernant la carte fournie.
+   *
+   * @param interaction L'interaction à l'origine de l'exécution de cette commande
+   * @param card La carte concernée
+   * @returns Le résultat de l'exécution de la commande
+   */
+  private async sendFaqEntries(
+    interaction: CommandInteraction,
+    card: ArkhamDBCard
+  ): Promise<IApplicationCommandResult> {
+    const faqEntries = await this.cardService.getCardFAQ(card);
+    if (faqEntries.length > 0) {
+      const ephemeral = interaction.options.getBoolean("ephemere") || false;
+      await interaction.reply({
+        content: faqEntries[0].text,
+        ephemeral,
+      });
+      return this.commandResult("FAQ envoyée");
+    } else {
+      await interaction.reply({
+        content: `Aucune entrée de FAQ pour la carte ${card.name}`,
+        ephemeral: true,
+      });
+      return this.commandResult("Aucune FAQ pour cette carte");
+    }
+  }
+
+  /**
+   * Envoie à l'utilisateur les cartes correspondant à sa recherche et parmi
+   * lesquelles il devra choisir pour ensuite obtenir les entrées de FAQ.
+   *
+   * @param interaction L'interaction à l'origine de l'exécution de cette commande
+   * @param cards Les cartes trouvées et parmi lesquelles il faudra choisir
+   */
+  private async sendCardChoicesThenFaqEntries(
+    interaction: CommandInteraction,
+    _cards: ArkhamDBCard[]
+  ): Promise<IApplicationCommandResult> {
+    await interaction.reply({
+      content: "Je ne sais pas encore faire cela",
+      ephemeral: true,
+    });
+    return this.commandResult("Non supporté");
+  }
+
+  /**
+   * Pour indiquer qu'aucune carte ne correspond à la recherche.
+   *
+   * @param interaction L'interaction à l'origine de l'exécution de cette commande
+   * @returns Le résultat de l'exécution de la commande
+   */
+  private async noCardMatched(
+    interaction: CommandInteraction
+  ): Promise<IApplicationCommandResult> {
+    await interaction.reply({
+      content: "Aucune carte ne correspond à cette recherche",
+      ephemeral: true,
+    });
+    return this.commandResult(
+      `Aucune carte ne correspond à la recherche ${
+        interaction.options.getString("recherche") || ""
+      }`
+    );
   }
 
   private commandResult(
