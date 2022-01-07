80bc4c9d9ef713a40e8a1fc909453b4c2ac7dd3f
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-07 18:14:28 +0100

    doc(RoleService): ajout doc

--- src/services/RoleService.ts
+++ src/services/RoleService.ts
@@ -5,7 +5,18 @@ import { BaseService } from "../base/BaseService";
 
 @Singleton
 @OnlyInstantiableByContainer
+/**
+ * Service permettant des opérations vis-à-vis des rôles sur un serveur Discord.
+ */
 export class RoleService extends BaseService {
+  /**
+   * Vérifie si le message fourni provient bien d'un membre ayant le rôle dont
+   * le nom est fourni.
+   *
+   * @param message Le message concerné
+   * @param roleName Le rôle vérifié
+   * @returns Vrai si le message provient bien d'un membre ayant le rôle indiqué
+   */
   public isMessageFromRole(message: Message, roleName: string): boolean {
     if (message.member) {
       return message.member.roles.cache.some((role) => role.name === roleName);
