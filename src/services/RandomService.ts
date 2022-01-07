ef925f4582354e589366da74b63fd28b370818ec
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-07 18:04:14 +0100

    doc(RandomService): ajout doc

--- src/services/RandomService.ts
+++ src/services/RandomService.ts
@@ -4,7 +4,18 @@ import { BaseService } from "../base/BaseService";
 
 @Singleton
 @OnlyInstantiableByContainer
+/**
+ * Un service pour tirer des nombres au hasard.
+ */
 export class RandomService extends BaseService {
+  /**
+   * Tirer un entier aléatoire compris entre le bornes inférieure et supérieure
+   * indiquées.
+   *
+   * @param min La borne minimum (incluse)
+   * @param max La borne maximuml (incluse)
+   * @returns Un nombre entier compris entre `min` et `max`
+   */
   public getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
