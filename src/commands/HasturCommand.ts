d533cef55681df84193a07ce0b964d6110ae5a0d
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-08 12:41:09 +0100

    doc(HasturCommand): ajout doc

--- src/commands/HasturCommand.ts
+++ src/commands/HasturCommand.ts
@@ -4,6 +4,7 @@ import { Inject } from "typescript-ioc";
 import { ICommand } from "../interfaces";
 import { EmojiService } from "../services/EmojiService";
 
+/** Ecouteur pour celui dont on ne doit pas prononcer le nom */
 export class HasturCommand implements ICommand {
   aliases = undefined;
   help = "Il ne faut pas prononcer son nom";
