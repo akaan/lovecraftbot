d7654fec1e5a46cf9118785dca5c8fd37f633514
Akaan Qualrus <akaan.qualrus@gmail.com>
2022-01-08 15:33:19 +0100

    v2.3.1

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -32,7 +32,7 @@ export class HelpCommand implements IApplicationCommand {
       ...commandDescriptions,
       {
         name: "Version",
-        value: "2.3.0",
+        value: "2.3.1",
         inline: true,
       },
       {
